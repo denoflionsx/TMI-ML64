401a0a7254dd9c154b0b50c9edaec7639c78d323
Denoflions <denvstheworld@gmail.com>
2020-01-04 10:00:31 -0500

    Add more events.

--- src/TMI/tmi.ts
+++ src/TMI/tmi.ts
@@ -3,9 +3,9 @@ import { Client, Options } from 'tmi.js';
 import fs from 'fs';
 import { bus, EventHandler } from "modloader64_api/EventHandler";
 
-class tmi implements IPlugin{
+class tmi implements IPlugin {
 
-    ModLoader!: IModLoaderAPI; 
+    ModLoader!: IModLoaderAPI;
     pluginName?: string | undefined;
     client!: Client;
     optsFile: string = "./TMI.json";
@@ -17,21 +17,100 @@ class tmi implements IPlugin{
     }
 
     @EventHandler("TMI:onMessage")
-    onMessage(evt: any){
+    onMessage(evt: any) {
+    }
+
+    @EventHandler("TMI:onCheer")
+    onCheer(evt: any) {
+    }
+
+    @EventHandler("TMI:onResub")
+    onResub(evt: any) {
+    }
+
+    @EventHandler("TMI:onGiftsub")
+    onGiftSub(evt: any) {
+    }
+
+    @EventHandler("TMI:onMysterysub")
+    onMysterySub(evt: any) {
+    }
+
+    @EventHandler("TMI:onSub")
+    onSub(evt: any) {
+    }
+
+    @EventHandler("TMI:onHost")
+    onHost(evt: any) {
+    }
+
+    @EventHandler("TMI:onRaid")
+    onRaid(evt: any) {
     }
 
     postinit(): void {
-        if (fs.existsSync(this.optsFile)){
+        if (fs.existsSync(this.optsFile)) {
             let opts: TwitchOpts = JSON.parse(fs.readFileSync(this.optsFile).toString());
             this.client = Client(opts);
             this.client.connect();
             this.client.on('message', (channel, tags, message, self) => {
-                if(self) return;
-                bus.emit("TMI:onMessage", {msg: message.toLowerCase(), tags: tags, reply: (msg: string)=>{
-                    this.client.say(channel, msg);
-                }});
+                if (self) return;
+                bus.emit("TMI:onMessage", {
+                    msg: message.toLowerCase(), tags: tags, reply: (msg: string) => {
+                        this.client.say(channel, msg);
+                    }
+                });
             });
-        }else{
+            this.client.on("cheer", (channel, tags, message) => {
+                bus.emit("TMI:onCheer", {
+                    msg: message.toLowerCase(), tags: tags, reply: (msg: string) => {
+                        this.client.say(channel, msg);
+                    }
+                });
+            });
+            this.client.on("resub", (channel, username, months, message, tags, methods) => {
+                bus.emit("TMI:onResub", {
+                    msg: message.toLowerCase(), username: username, tags: tags, method: methods, months: months, reply: (msg: string) => {
+                        this.client.say(channel, msg);
+                    }
+                });
+            });
+            this.client.on("subgift", (channel, username, streakMonths, recipient, methods, tags) => {
+                bus.emit("TMI:onGiftsub", {
+                    msg: "", tags: tags, gifter: username, recipient: recipient, method: methods, streak: streakMonths, reply: (msg: string) => {
+                        this.client.say(channel, msg);
+                    }
+                });
+            });
+            this.client.on("submysterygift", (channel, username, numbOfSubs, methods, tags) => {
+                bus.emit("TMI:onMysterysub", {
+                    msg: "", tags: tags, gifter: username, num: numbOfSubs, method: methods, reply: (msg: string) => {
+                        this.client.say(channel, msg);
+                    }
+                });
+            });
+            this.client.on("subscription", (channel, username, method, message, tags) => {
+                bus.emit("TMI:onSub", {
+                    msg: message, username: username, tags: tags, method: method, reply: (msg: string) => {
+                        this.client.say(channel, msg);
+                    }
+                });
+            });
+            this.client.on("hosted", (channel, username, viewers, autohost) => {
+                bus.emit("TMI:onHost", {
+                    msg: "", username: username, viewers: viewers, autohost: autohost, reply: (msg: string) => {
+                        this.client.say(channel, msg);
+                    }
+                });
+            });
+            this.client.on("raided", (channel, username, viewers) => {
+                bus.emit("TMI:onRaid", {
+                    msg: "", username: username, viewers: viewers, reply: (msg: string) => {
+                        this.client.say(channel, msg);
+                    }
+                });
+            });
+        } else {
             this.ModLoader.logger.error("[TMI]: THIS MOD WILL NOT FUNCTION UNTIL YOU CLOSE MODLOADER AND EDIT TMI.JSON!");
             fs.writeFileSync(this.optsFile, JSON.stringify(new TwitchOpts("", "", []), null, 2));
         }
@@ -44,15 +123,15 @@ class tmi implements IPlugin{
 
 module.exports = tmi;
 
-class TwitchOpts implements Options{
-    options: any = {debug: true};
+class TwitchOpts implements Options {
+    options: any = { debug: true };
     identity: any = {
         username: "",
         password: ""
     };
     channels: Array<string> = [];
 
-    constructor(username: string, password: string, channel: Array<string>){
+    constructor(username: string, password: string, channel: Array<string>) {
         this.identity["username"] = username;
         this.identity["password"] = password;
         this.channels = channel;
